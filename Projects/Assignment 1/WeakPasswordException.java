9ffaa75e98a163c4b1bd908486b5a78451865604
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-06-10 23:37:10 -0400

    Add files via upload

---
+++ Projects/Assignment 1/WeakPasswordException.java
@@ -0,0 +1,11 @@
+
+public class WeakPasswordException extends RuntimeException {
+
+	public WeakPasswordException() {
+		super("The password is OK but weak - it contains fewer than 10 characters.");
+	}
+
+	public WeakPasswordException(String message) {
+		super(message);
+	}
+}
